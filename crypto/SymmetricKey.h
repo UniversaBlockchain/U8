d7d0425c2ef04534a66722f28bd8cfa5268f3fbf
Leonid Novikov <flint.emerald@gmail.com>
2019-02-04 10:12:31 +0300

    doxygen SymmetricKey

--- crypto/SymmetricKey.h
+++ crypto/SymmetricKey.h
@@ -11,16 +11,45 @@
 class SymmetricKey {
 
 public:
+
+    /**
+     * Create random symmetric key (AES256, CTR)
+     */
     SymmetricKey();
+
+    /**
+     * Create symmetric key from a packed data
+     */
     SymmetricKey(const byte_vector& key);
+
+    /**
+     * \see SymmetricKey(const byte_vector& key)
+     */
     SymmetricKey(void* keyData, size_t keyDataSize);
 
+    /**
+     * Returns packed key data
+     */
     byte_vector pack();
 
+    /**
+     * Encrypt data using AE (EtA) with HMAC based on SHA256
+     */
     byte_vector etaDecrypt(const byte_vector& data);
+
+    /**
+     * \see byte_vector etaDecrypt(const byte_vector& data)
+     */
     byte_vector etaDecrypt(void* data, size_t size);
 
+    /**
+     * Decrypt data using AE (EtA) with SHA256-based HMAC
+     */
     byte_vector etaEncrypt(const byte_vector& data);
+
+    /**
+     * \see byte_vector etaEncrypt(const byte_vector& data)
+     */
     byte_vector etaEncrypt(void* data, size_t size);
 
 private:
