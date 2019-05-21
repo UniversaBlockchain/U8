ab86663c75d53dcc09c27580de62166bac6e985e
Tairov Dmitriy <tairov_dmitriy@rambler.ru>
2019-05-21 17:39:16 +0300

    init main.js

---
+++ jslib/main.js
@@ -0,0 +1,23 @@
+
+async function main(...args) {
+    new Main(...args);
+}
+
+class Main {
+
+    constructor(...args) {
+        //OptionParser(...args)
+
+        //options.has("config")) {
+        //  loadNodeConfig();
+
+        //startClientHttpServer();
+
+        //startNode();
+    }
+}
+
+///////////////////////////
+//EXPORTS
+///////////////////////////
+module.exports = {Main};
