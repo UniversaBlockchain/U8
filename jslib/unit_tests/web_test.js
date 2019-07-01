44d620a7ccf7b34c281ee069a546b5c66fb3cef8
Roman Uskov <roman.uskov@gmail.com>
2019-07-01 15:40:10 +0300

    big payload test added

--- jslib/unit_tests/web_test.js
+++ jslib/unit_tests/web_test.js
@@ -2,6 +2,7 @@ import {expect, unit, assert, assertSilent} from 'test'
 import {HttpServer} from 'web'
 import * as tk from 'unit_tests/test_keys'
 const Boss = require('boss.js');
+const t = require('tools.js');
 const ItemResult = require('itemresult.js');
 
 unit.test("hello web", async () => {
@@ -145,6 +146,43 @@ unit.test("http secure endpoints", async () => {
     let rps = (receiveCounter*1000/dt).toFixed(0);
     console.logPut(" rps=" + rps + " ");
     assert(receiveCounter == countToSend);
+
+    await httpClient.stop();
+    await httpServer.stopServer();
+});
+
+
+
+unit.test("big payload", async () => {
+    let httpServer = new network.HttpServer("0.0.0.0", 8080, 1, 20);
+    let nodeKey = new crypto.PrivateKey(atob("JgAcAQABvID6D5ZdM9EKrZSztm/R/RcywM4K8Z4VBtX+NZp2eLCWtfAgGcBCQLtNz4scH7dPBerkkxckW6+9CLlnu/tgOxvzS6Z1Ec51++fVP9gaWbBQe9/dSg7xVPg5p9ibhfTB+iRXyevCkNj0hrlLyXl1BkPjN9+lZfXJsp9OnGIJ/AaAb7yA99E65gvZnbb3/oA3rG0pM45af6ppZKe2HeiAK+fcXm5KTQzfTce45f/mJ0jsDmFf1HFosS4waXSAz0ZfcssjPeoF3PuXfJLtM8czJ55+Nz6NMCbzrSk6zkKssGBieYFOb4eG2AdtfjTrpcSSHBgJpsbcmRx4bZNfBAZPqT+Sd20="));
+    let clientKey = await crypto.PrivateKey.generate(2048);
+    // console.log("clientKey: " + btoa(new crypto.PublicKey(clientKey).fingerprints));
+    httpServer.initSecureProtocol(nodeKey);
+    let counter = 0;
+    let unsRateDbg = 333;
+    httpServer.addSecureEndpoint("testEndpoint", async (reqParams, clientPublicKey) => {
+        return {hash: crypto.HashId.of(reqParams.testData)};
+    });
+    httpServer.startServer();
+
+
+    let httpClient = new network.HttpClient("http://localhost:8080", 32, 64);
+    await httpClient.start(clientKey, new crypto.PublicKey(nodeKey));
+
+
+    let testData = t.randomBytes(10000);
+
+    httpClient.command("testEndpoint", {testData: testData}, async (resp) => {
+        let hashOk = t.valuesEqual(resp.hash,crypto.HashId.of(testData));
+        console.log("hash ok: " + hashOk);
+        assert(hashOk);
+    }, error => {
+        console.log("exception: " + error);
+    });
+
+    await sleep(2000);
+
 
     await httpClient.stop();
     await httpServer.stopServer();
