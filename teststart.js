89a60a0a529d4bd04a9bddb0b9c79bdf19b50510
sergeych <real.sergeych@gmail.com>
2019-07-15 22:04:44 +0200

    big reworking of V8-u8 core to single thread dispatcher - bug show

--- teststart.js
+++ teststart.js
@@ -5,8 +5,13 @@
 //     return 202;
 // }
 function main() {
-    console.log("Welcome!");
-    console.log("Done!");
-    __debug_throw(0, "te1");
+    try {
+        console.log("Welcome!");
+        __debug_throw(0, "te1");
+        console.log("Done!");
+    }
+    catch(e) {
+        console.log("we caught: "+e);
+    }
     return 101;
 }
