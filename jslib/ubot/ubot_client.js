66a4b0bcaced181f241bb7c0aa09236a9305b6d9
kalinkineo <kalinkineo@gmail.com>
2019-09-27 03:55:59 +0300

    UBotNet: client documentation

--- jslib/ubot/ubot_client.js
+++ jslib/ubot/ubot_client.js
@@ -51,6 +51,13 @@ class UBotClient {
         this.httpNodeClients = [];
     }
 
+    /**
+     * The method starts immediately after the constructor.
+     * Connects to the Universa network by its topology.
+     *
+     * @async
+     * @return {Promise<UBotClient>}
+     */
     async start() {
         let tb = await new TopologyBuilder().build(this.topologyInput, this.topologyCacheDir);
         this.topology = tb.topology;
@@ -74,6 +81,11 @@ class UBotClient {
         return this;
     }
 
+    /**
+     * Complete client work and also closes all connections.
+     *
+     * @async
+     */
     async shutdown() {
         if (this.httpNodeClients.length === 0)
             await this.httpNodeClient.stop();
@@ -83,6 +95,12 @@ class UBotClient {
             await this.httpUbotClient.stop();
     }
 
+    /**
+     * Connects to all nodes of the Universa network.
+     *
+     * @private
+     * @async
+     */
     async connectAllNodes() {
         for (let i = 0; i < this.nodes.length; i++) {
             if (this.topology[i].number === this.httpNodeClient.nodeNumber)
@@ -96,6 +114,13 @@ class UBotClient {
         }
     }
 
+    /**
+     * Connects to a random UBot from the session pool.
+     *
+     * @private
+     * @async
+     * @param pool
+     */
     async connectUbot(pool) {
         if (this.topologyUBotNet == null)
             throw new UBotClientException("UBotNet topology not initialized");
@@ -126,6 +151,14 @@ class UBotClient {
         await this.httpUbotClient.start(this.clientPrivateKey, this.ubotPublicKey);
     }
 
+    /**
+     * Executes the session request command on the Universa network on the node to which the client was connected,
+     * in the Start method.
+     *
+     * @private
+     * @async
+     * @return {Promise<Object>} command result.
+     */
     async getSession(command, params) {
         let sessionData = await new Promise(async (resolve, reject) =>
             await this.httpNodeClient.command(command, params,
@@ -146,6 +179,13 @@ class UBotClient {
         return sessionData.session;
     }
 
+    /**
+     * Executes a command on the Universa network on the node to which the client was connected, in the Start method.
+     *
+     * @private
+     * @async
+     * @return {Promise<Object>} command result.
+     */
     async askSession(command, params) {
         let data = await new Promise(async (resolve, reject) =>
             await this.httpNodeClient.command(command, params,
@@ -163,6 +203,13 @@ class UBotClient {
         return data;
     }
 
+    /**
+     * Executes a command on all nodes of the Universe network.
+     *
+     * @private
+     * @async
+     * @return {Promise<Object>} command result.
+     */
     async askSessionOnAllNodes(command, params) {
         if (this.httpNodeClients.length === 0)
             await this.connectAllNodes();
@@ -183,6 +230,13 @@ class UBotClient {
         return data;
     }
 
+    /**
+     * Creates a new session for the cloud method.
+     *
+     * @private
+     * @async
+     * @return {UBotSession} session.
+     */
     async createSession(requestContract) {
         let session = await this.getSession("ubotCreateSession",
             {packedRequest: await requestContract.getPackedTransaction()});
@@ -219,6 +273,19 @@ class UBotClient {
         return new UBotSession(session, this, requestContract.state.data.executable_contract_id);
     }
 
+    /**
+    * Start cloud method.
+    * Requests the creation of a session with a randomly selected pool using the Request contract.
+    * Creates a session with the id of the Request contract or throws an exception if the session is already created
+    * for another Request contract under the same executable contract.
+    * Waits until the session is assigned an id and a pool is calculated from it.
+    * Then he asks for a contract with the registry and topology of UBots,
+    * connects to a random UBots from the pool on which he runs the cloud method.
+    *
+    * @param {Contract} requestContract - The Request contract.
+    * @async
+    * @return {UBotSession} session.
+    */
     async startCloudMethod(requestContract) {
         if (this.httpUbotClient != null)
             throw new UBotClientException("Ubot is connected to the pool. First disconnect from the pool");
@@ -257,6 +324,17 @@ class UBotClient {
         return session;
     }
 
+    /**
+     * Verification of the session on the id of the Executable contract and the Request contract.
+     *
+     * @param {HashId} executableContractId - The Executable contract id.
+     * @param {HashId} requestContractId - The Request contract id.
+     * @param ubotNumber
+     * @param ubot
+     * @private
+     * @async
+     * @return {Promise<UBotSession>}.
+     */
     async checkSession(executableContractId, requestContractId, ubotNumber, ubot) {
         let session = await this.getSession("ubotGetSession",
             {executableContractId: executableContractId});
@@ -290,6 +368,14 @@ class UBotClient {
         return new UBotSession(session, this, executableContractId, ubot);
     }
 
+    /**
+     * Gets the current state of the cloud method, if the state is FINISHED,
+     * then the result of the cloud method execution is in the returned data.
+     *
+     * @param {HashId} requestContractId - The Request contract id.
+     * @async
+     * @return {Promise<Object>} fields in the object: state - method status, result - method result, errors - error list.
+     */
     async getStateCloudMethod(requestContractId) {
         if (this.httpUbotClient == null)
             throw new UBotClientException("Ubot HTTP client is not initialized");
@@ -302,6 +388,14 @@ class UBotClient {
         );
     }
 
+    /**
+     * It waits for the completion of the cloud method and
+     * returns its final state with the result of the cloud method.
+     *
+     * @param {HashId} requestContractId - The Request contract id.
+     * @async
+     * @return {Promise<Object>} fields in the object: state - method status, result - method result, errors - error list.
+     */
     async waitCloudMethod(requestContractId) {
         let state = await this.getStateCloudMethod(requestContractId);
 
@@ -314,6 +408,13 @@ class UBotClient {
         return state;
     }
 
+    /**
+     * Disconnects from random UBots from the pool selected to execute the cloud method.
+     * But it remains connected to the nodes of the Universa network. This allows you to start a new cloud method
+     * on a new random pool using the startCloudMethod method.
+     *
+     * @async
+     */
     async disconnectUbot() {
         if (this.httpUbotClient == null)
             throw new UBotClientException("Ubot is not connected to the pool");
