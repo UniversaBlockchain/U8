c4bd6f64973f9c8aa9e72f70d7778b9b2e6a8a2f
Leonid Novikov <flint.emerald@gmail.com>
2019-10-09 11:51:58 +0300

    added ubot.js

---
+++ ubot.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019-present Sergey Chernov, iCodici S.n.C, All Rights Reserved.
+ */
+
+const UBotMain = require("ubot/ubot_main").UBotMain;
+
+const Boss = require('boss.js');
+const DefaultBiMapper = require("defaultbimapper").DefaultBiMapper;
+const Contract = require("contract").Contract;
+const t = require("tools");
+
+async function main(args) {
+
+    let ubotMain = new UBotMain(...args);
+    await ubotMain.start();
+
+    await sleep(100000000000);
+
+    await main.shutdown();
+}
