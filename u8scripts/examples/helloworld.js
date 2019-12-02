79e47c1a87e9fb827be980de68f1b089e781a75e
Alex Myodov <amyodov@gmail.com>
2019-12-02 12:58:36 +0300

    Minor fix for helloworld.js u8 script.

--- u8scripts/examples/helloworld.js
+++ u8scripts/examples/helloworld.js
@@ -1,25 +1,15 @@
+/*
+ * Copyright (c) 2019-present Sergey Chernov, iCodici S.n.C, All Rights Reserved.
+ */
+
 // Launch as:
 //   u8 helloworld.js
 // or
 //   u8 helloworld.js yourname
-
-// async function main(args) {
-//     console.log(`Hello, ${(args.length > 0)? args[0] : 'world'}!`);
-//     return 0;
-// }
 //
-//
-// Launch as:
-//   u8 helloworld.js
-// or
-//   u8 helloworld.js yourname
-
-async function f() {
-    await sleep(5 * 1000);
-    return 5;
-}
+// See details at https://kb.universablockchain.com/u8_hello_world/221
 
 async function main(args) {
     console.log(`Hello, ${(args.length > 0)? args[0] : 'world'}!`);
-    return f();
+    return 0;
 }
