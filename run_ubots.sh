636a13522da2d6b64124a3d52de304aa1da0085c
kalinkineo <kalinkineo@gmail.com>
2019-12-02 18:57:50 +0300

    fix run_ubots.sh

--- run_ubots.sh
+++ run_ubots.sh
@@ -4,5 +4,5 @@ mkdir ubot_logs
 cd build-ubot
 for i in `seq 0 29`
 do
-    nohup ./u8 ../ubot.js --config ../test/config/ubot_config/ubot$i/ > ../ubot_logs/log$i.txt 2> ../ubot_logs/err.$i.txt  &
+    nohup ./u8 ../u8scripts/ubotserver/ubotserver.js --config ../test/config/ubot_config/ubot$i/ > ../ubot_logs/log$i.txt 2> ../ubot_logs/err.$i.txt  &
 done
