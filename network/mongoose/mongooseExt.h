f3a229db7b34cda5df61260f22faae2e36f9123a
Leonid Novikov <flint.emerald@gmail.com>
2019-12-18 19:24:59 +0300

    mongoose: support ipv6

--- network/mongoose/mongooseExt.h
+++ network/mongoose/mongooseExt.h
@@ -7,6 +7,25 @@
 
 #include "mongoose.h"
 
+//////////////////////////////////////////////////////////
+//// for IPv6 support, do manually fix in mongoose.c
+//// (still not fixed in in 6.16)
+/*
+
+in mg_socket_if_connect_tcp
+-- nc->sock = socket(AF_INET; SOCK_STREAM, proto);
+...
+-- rc = connect(nc->sock, &sa->sa,(sizeof) sa->sa.sin);
+
+++ socklen_t sa_len = (sa->sa.sa_family ==AF_INET) ? sizeof(sa->sin) : sizeof(sa->sin6);
+++ nc->sock = socket(sa->sa.sa_family;SOCK_STREAM, proto);
+...
+++  rc = connect(nc->sock, &sa->sa, sa_len);
+
+*/
+//// for IPv6 support, do manually fix in mongoose.c
+//////////////////////////////////////////////////////////
+
 struct mg_connection *mg_connect_http_base(
     struct mg_mgr *mgr, MG_CB(mg_event_handler_t ev_handler, void *user_data),
     struct mg_connect_opts opts, const char *scheme1, const char *scheme2,
