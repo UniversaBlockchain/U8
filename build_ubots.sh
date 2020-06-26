461ef19bcac83938d32661225bdc32c9d32cc55e
Roman Uskov <roman.uskov@gmail.com>
2020-06-26 20:24:13 +0300

    redundant ubot build scripts removed

--- build_ubots.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./stop_ubots.sh
-rm -rf build-ubot
-cmake -S . -B build-ubot
-cmake --build build-ubot --target u8 -- -j
--- run_ubots.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./stop_ubots.sh
-mkdir ubot_logs
-cd build-ubot
-for i in `seq 0 29`
-do
-    nohup ./u8 ../u8scripts/ubotserver/ubotserver.js --config ../test/config/ubot_config/ubot$i/ > ../ubot_logs/log$i.txt 2> ../ubot_logs/err.$i.txt  &
-done
