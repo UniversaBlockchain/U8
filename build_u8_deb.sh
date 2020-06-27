53114c5e926863d4dcebae76065d23e692189c26
Leonid Novikov <flint.emerald@gmail.com>
2020-06-27 11:40:01 +0300

    added build_u8_deb.sh script

---
+++ build_u8_deb.sh
@@ -0,0 +1,6 @@
+cp ./cmake-build-monolith-release/u8 ./u8-deb/usr/bin/u8
+fakeroot dpkg-deb --build u8-deb
+rm ./u8-deb/usr/bin/u8
+
+# check package: lintian u8-deb.deb
+# install package: sudo apt -f install ./u8-deb.deb
