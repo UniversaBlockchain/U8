5eb3a3ab167879fa8d3bb948e30f0c87a840683a
Leonid Novikov <flint.emerald@gmail.com>
2020-07-21 17:19:07 +0300

    contract and roles tests moved to jslib

--- u8scripts/examples/testcore.js
+++ u8scripts/examples/testcore.js
@@ -17,19 +17,25 @@ const BossStreams = require('boss_streams.js');
 
 import {expect, unit} from 'test'
 
-require('unit_tests/services/uns_register_test');
-require('unit_tests/deltas_test');
-require('unit_tests/worker_tests');
-require('unit_tests/boss_test');
-require('unit_tests/crypto_test');
-require('unit_tests/pseudo_random_test');
-require('unit_tests/network_test');
-require('unit_tests/file_tests');
-require('unit_tests/pg_test');
-require('unit_tests/collection_test');
-require('unit_tests/asyncevent_test');
-require('unit_tests/lock_test');
-require('unit_tests/web_test');
+// require('unit_tests/services/uns_register_test');
+require('unit_tests/contract_test');
+require('unit_tests/role_test');
+require('unit_tests/simplerole_test');
+require('unit_tests/listrole_test');
+require('unit_tests/linkrole_test');
+
+// require('unit_tests/deltas_test');
+// require('unit_tests/worker_tests');
+// require('unit_tests/boss_test');
+// require('unit_tests/crypto_test');
+// require('unit_tests/pseudo_random_test');
+// require('unit_tests/network_test');
+// require('unit_tests/file_tests');
+// require('unit_tests/pg_test');
+// require('unit_tests/collection_test');
+// require('unit_tests/asyncevent_test');
+// require('unit_tests/lock_test');
+// require('unit_tests/web_test');
 
 async function main(args) {
     await unit.perform(args);
